c4e61f839e74ab3cd3176210ad644ef65234940b
Mitsuhiro Yamashita <yamamanx@gmail.com>
2018-06-07 20:27:39 +0900

    repair

--- camera/processor/qr_detector.py
+++ camera/processor/qr_detector.py
@@ -4,16 +4,16 @@ from datetime import datetime
 import numpy as np
 import cv2
 from pyzbar import pyzbar
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+#from picamera.array import PiRGBArray
+#from picamera import PiCamera
 
 
 class QRDetector(object):
     def __init__(self, flip=False):
-        self.camera = PiCamera()
-        self.camera.resolution = (640, 480)
-        self.camera.framerate = 32
-        self.rawCapture = PiRGBArray(self.camera, size=(640, 480))
+        #self.camera = PiCamera()
+        #self.camera.resolution = (640, 480)
+        #self.camera.framerate = 32
+        #self.rawCapture = PiRGBArray(self.camera, size=(640, 480))
 
         self.vs = PiVideoStream(resolution=(800, 608)).start()
         self.flip = flip
@@ -26,8 +26,8 @@ class QRDetector(object):
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-    #def __del__(self):
-        #self.vs.stop()
+    def __del__(self):
+        self.vs.stop()
 
     def flip_if_needed(self, frame):
         if self.flip:
@@ -35,17 +35,17 @@ class QRDetector(object):
         return frame
 
     def get_frame(self):
-        #frame = self.flip_if_needed(self.vs.read())
-        #frame = self.process_image(frame)
+        frame = self.flip_if_needed(self.vs.read())
+        frame = self.process_image(frame)
         #ret, jpeg = cv2.imencode('.jpg', frame)
         #return jpeg.tobytes()
 
-        self.camera.capture(self.rawCapture, format="bgr", use_video_port=True)
-        frame = self.rawCapture.array
+        #self.camera.capture(self.rawCapture, format="bgr", use_video_port=True)
+        #frame = self.rawCapture.array
         decoded_objs = self.decode(frame)
         frame = self.display(frame, decoded_objs)
         ret, jpeg = cv2.imencode('.jpg', frame)
-        self.rawCapture.truncate(0)
+        #self.rawCapture.truncate(0)
 
         return jpeg.tobytes()
         
